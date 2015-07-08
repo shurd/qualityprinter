9a01c0669541158474c5c05c854931775a702b5a
shurd <sam.p.hurd@vanderbilt.edu>
2015-07-08 14:53:38 -0500

    corrected pronterface macro

--- quality_print.py
+++ quality_print.py
@@ -1,14 +1,18 @@
+!import os
 !notRunBefore = True
 !while(self.p.printing and notRunBefore):
      !if (self.curlayer>0.350 and notRunBefore):
-          !self.update_pos()
-          !currentPositionX = self.current_pos[0]
-          !currentPositionY = self.current_pos[1]
-          !currentPositionZ = self.current_pos[2]
           !self.pause() #1179
           !self.onecmd('G0 X200 Y250 Z190 F2000')
-          !time.sleep(15)
-          run_script python #insert directory where the parse python script is located (parsetest directory)
-          !self.pause()
+          !time.sleep(15) #analysis here, may not work
+          #insert path to __main__.py on your machine
+          !exitcode = os.system("<path to __main__.py>")
+          !if exitcode == 10: #end print
+               !print "Error too high. Stopping print"
+               !return
+          !elif exitcode == 15:
+               !print "Error within accepted bounds. Continuing print"
+               !self.onecmd('G0 X200 Y250 Z10 F2000')
+               !self.pause() #1179
           !notRunBefore = False
 !print "Continuing Print"
